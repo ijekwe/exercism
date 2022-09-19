4a5ea2c029ab33b86661e69bfa3cb4ebdebe3d32
iheanyiekwe <contactiheanyi@gmail.com>
2022-09-18 21:48:49 -0500

    hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,16 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	dist := 0
+	if len(a) != len(b) {
+		return 0, errors.New("Input Strings must be same length")
+	}
+	for i, _ := range a {
+		if a[i] != b[i] {
+			dist += 1
+		}
+	}
+	return dist, nil
 }
